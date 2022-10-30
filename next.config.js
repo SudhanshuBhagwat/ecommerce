2c169a42d33f54071d0826b9b938d5c7a90a25ca
SudhanshuBhagwat <sudhanshubhagwat3@gmail.com>
2022-10-30 23:27:42 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    providerImportSource: "@mdx-js/react",
+  },
+});
+
+module.exports = withMDX({
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  ...nextConfig,
+});
